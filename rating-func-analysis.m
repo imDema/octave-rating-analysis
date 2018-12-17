47ab2ced15e79d6c8d91042ed9850ca3f458fbaf
imDema <20422970+imDema@users.noreply.github.com>
2018-12-17 19:01:23 +0100

    Init

---
+++ rating-func-analysis.m
@@ -0,0 +1,73 @@
+clear
+
+###PARAMS
+#Function surface
+maxLikes=80;
+maxDislikes=40;
+surfDiv=2;
+
+
+#Level analysis
+maxNum=40;
+levelN=40;
+ratioN=25;
+evolDiv=1;
+
+###Function
+
+function rating = rank(_pos,_neg);
+  neg = _neg*2;
+  n = _pos.+neg+6;
+  z = 1.51;
+  p = (_pos+3)./n;
+  rating = (p.+(z.*z)./(2.*n).-z.*sqrt((p.*(1-p).+z.*z./(4.*n))./n ))./(1.+z.*z./n);
+endfunction
+
+##########
+
+likes=linspace(0,maxLikes,(maxLikes/surfDiv)+1);
+dislikes=linspace(0,maxDislikes,(maxDislikes/surfDiv)+1);
+
+ratio = linspace(0,1,ratioN+1);
+num = linspace(1,maxNum,(maxNum/evolDiv)+1);
+
+[_ratio,_num]=meshgrid(ratio,num);
+[_likes,_dislikes]=meshgrid(likes,dislikes);
+
+_pos=_ratio.*_num;
+_neg=(1-_ratio).*_num;
+
+fevolution = rank(_pos,_neg);
+fsurface = rank(_likes,_dislikes);
+
+figure(1)
+
+s1 = surfc(_likes,_dislikes,fsurface);
+xlabel "Likes";
+ylabel "Dislikes";
+zlabel "Rating";
+title "z = Rating(Likes,Dislikes)"
+M = max(maxLikes,maxLikes);
+axis([0,M,0,M],"square");
+view (225,15);
+
+figure (2)
+
+s2 = contour(_ratio*100,fevolution,_num,levelN);
+xlabel '% of likes';
+ylabel 'Rating';
+title("Evoluzione della curva del rating in funzione di like% all'aumentare dei voti totali");
+axis([0,100,0,1],"square");
+c=colorbar("westoutside");
+set(c,"ylabel","tot N. of votes");
+
+figure(3)
+
+s3 = contour(_num,_ratio*100,fevolution,levelN);
+xlabel 'N. of total votes';
+ylabel '% of likes';
+title("Linee di livello del rating all'aumentare del nuemro di voti");
+axis("square");
+c=colorbar("westoutside");
+set(c,"ylabel","Rating");
+
